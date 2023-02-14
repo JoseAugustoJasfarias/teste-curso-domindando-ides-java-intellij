c3e18ebc8202d13847a64348970fa86296c89a1c
Jose Augusto <josejasfarias@gmail.com>
2023-02-13 22:42:08 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+       /* double a = 5;
+        double b = 3;
+
+        System.out.println("Hello World!" + (a + b));*/
+    }
+
+}
