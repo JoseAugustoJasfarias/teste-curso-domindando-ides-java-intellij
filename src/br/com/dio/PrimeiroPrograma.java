6fb9255352f4a3cf58bb174118cb897d656c83b2
Jose Augusto <josejasfarias@gmail.com>
2023-02-13 22:54:22 -0300

    Cliando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,16 +2,70 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
 
         System.out.println(gato);
-       /* double a = 5;
+
+
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
+        /* double a = 5;
         double b = 3;
 
         System.out.println("Hello World!" + (a + b));*/
     }
 
+}
+
+class Livro {
+    private  String nome;
+    private  Integer numPaginas;
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
 }
